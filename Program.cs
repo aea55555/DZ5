967b677e3ff3c29094ed34d7f0781c307218dbab
aea55555 <118430414+aea55555@users.noreply.github.com>
2023-06-30 19:41:45 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -59,8 +59,7 @@
 // {
 //     int sum = 0;
 //     for (int i = 1; i < tempArray.Length; i+=2)
-//         //if (tempArray[i] == i)
-//             sum += tempArray[i];
+//          sum += tempArray[i];
 //     return sum;
 // }
 
